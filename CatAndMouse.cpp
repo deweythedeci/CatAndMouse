fbd931098c600f5219c50403a41fb0fb7ab11f35
deweythedeci <78463253+deweythedeci@users.noreply.github.com>
2022-05-15 10:30:28 -0700

    code works now

--- CatAndMouse.cpp
+++ CatAndMouse.cpp
@@ -9,9 +9,8 @@ struct Game;
 bool contains(vector<GameState> v, GameState a);
 bool contains(vector<int> v, int a);
 int best_cat_move(Game g, Board b);
-int cat_move_outcome(Game g, Board b, int m);
+int move_outcome(Game g, Board b, int m);
 int best_mouse_move(Game g, Board b);
-int mouse_move_outcome(Game g, Board b, int m);
 
 struct Board {
     vector<vector<int>> edges; // graph
@@ -43,22 +42,40 @@ struct Game {
     GameState state; // current game state
     vector<GameState> hist_c; // history of states where it's the cat's turn
     vector<GameState> hist_m; // history of states where it's the mouse's turn
+    int hole;
+    int turn = 1; // 0 = cat's turn, 1 = mouse's turn
 
-    Game(int c, int m) : state(c, m) {}
+    Game(int c, int m, int h) : state(c, m) {
+        hole = h;
+    }
     
-    // return 1 if the move results in a draw
+    // returns the resulting game state
+    // 0 = no change, 1 = cat win, 2 = mouse win, 3 = draw
     int cat_move(int n){
         state.cat = n;
-        if(contains(hist_m, state)){
+        turn = 1;
+        if(state.cat == state.mouse){
             return 1;
+        }
+        if(contains(hist_m, state)){
+            return 3;
         }
         hist_m.push_back(state);
         return 0;
     }
 
+    // returns the resulting game state
+    // 0 = no change, 1 = cat win, 2 = mouse win, 3 = draw
     int mouse_move(int n){
         state.mouse = n;
+        turn = 0;
+        if(state.mouse == hole){
+            return 2;
+        }
         if(contains(hist_c, state)){
+            return 3;
+        }
+        if(state.cat == state.mouse){
             return 1;
         }
         hist_c.push_back(state);
@@ -89,19 +106,23 @@ int best_cat_move(Game g, Board b){
     int draw = -1; // move that results in draw
     // go through all possible moves
     for(int m : b.edges.at(g.state.cat)){
+        // if it's the hole the cat can't move there
+        if(g.hole == m){
+            continue;
+        }
         // if the move wins pick it
         if(g.state.mouse == m){
             return m;
         }
         else{
             // check the move's outcome
-            int out = cat_move_outcome(g, b, m);
+            int out = move_outcome(g, b, m);
             // if the move wins pick it
             if(out == 1){
                 return m;
             }
             // if the move draws save it
-            else if(out == 2){
+            else if(out == 3){
                 draw = m;
             }
         }
@@ -116,27 +137,17 @@ int best_cat_move(Game g, Board b){
     }
 }
 
-// returns the outcome of the cat's move
-// 0 = loss, 1 = win, 2 = draw
-int cat_move_outcome(Game g, Board b, int m){
-    // make the move
-    if(g.cat_move(m)){
-        return 2;
+// returns the outcome a move
+// 1 = cat win, 2 = mouse win, 3 = draw
+int move_outcome(Game g, Board b, int m){
+    if(g.turn){
+        int out = g.mouse_move(m);
+        if(out){ return out; }
+        return move_outcome(g, b, best_cat_move(g, b));
     }
-    // check if the mouse is about to win
-    if(contains(b.edges.at(g.state.mouse), b.hole)){
-        return 0;
-    }
-    // make the mouse's best move
-    if(g.mouse_move(best_mouse_move(g, b))){
-        return 2;
-    }
-    // check if the cat is about to win
-    if(contains(b.edges.at(g.state.cat), g.state.mouse)){
-        return 1;
-    }
-    // make the cat's best move and check that outcome
-    return cat_move_outcome(g, b, best_cat_move(g, b));
+    int out = g.cat_move(m);
+    if(out){ return out; }
+    return move_outcome(g, b, best_mouse_move(g, b));
 }
 
 // returns the optimal move for the mouse
@@ -150,13 +161,13 @@ int best_mouse_move(Game g, Board b){
         }
         else{
             // check the moves outcome
-            int out = mouse_move_outcome(g, b, m);
+            int out = move_outcome(g, b, m);
             // if the move wins pick it
-            if(out == 1){
+            if(out == 2){
                 return m;
             }
             // if the move draws save it
-            else if(out == 2){
+            else if(out == 3){
                 draw = m;
             }
         }
@@ -171,47 +182,25 @@ int best_mouse_move(Game g, Board b){
     }
 }
 
-// returns the outcome of the mouse's move
-// 0 = loss, 1 = win, 2 = draw
-int mouse_move_outcome(Game g, Board b, int m){
-    // make the move
-    if(g.mouse_move(m)){
-        return 2;
-    }
-    // check if the cat is about to win
-    if(contains(b.edges.at(g.state.cat), g.state.mouse)){
-        return 0;
-    }
-    // make the cat's best move
-    if(g.cat_move(best_cat_move(g, b))){
-        return 2;
-    }
-    // check if the mouse is about to win
-    if(contains(b.edges.at(g.state.mouse), b.hole)){
-        return 1;
-    }
-    // make the mouse's best move and check that outcome
-    return mouse_move_outcome(g, b, best_mouse_move(g, b));
-}
-
 int main(){
-    vector<vector<int>> vect;
-    vect.at(0).push_back(2);
-    vect.at(0).push_back(5);
-    vect.at(1).push_back(3);
-    vect.at(2).push_back(0);
-    vect.at(2).push_back(4);
-    vect.at(2).push_back(5);
-    vect.at(3).push_back(1);
-    vect.at(3).push_back(4);
-    vect.at(3).push_back(5);
-    vect.at(4).push_back(2);
-    vect.at(4).push_back(3);
-    vect.at(5).push_back(0);
-    vect.at(5).push_back(2);
-    vect.at(5).push_back(3);
+    vector<vector<int>> vect {{2,5},{3},{0,4,5},{1,4,5},{2,3},{0,2,3}};
     Board b = Board(vect, 0);
-    Game g = Game(2, 1);
-    cout << mouse_move_outcome(g, b, best_mouse_move(g, b)) << endl;
+    Game g = Game(2, 1, 0);
+    int gs = 0;
+    while(!gs){
+        cout << "mouse's turn" << endl;
+        cout << "cat: " << g.state.cat << endl;
+        cout << "mouse: " << g.state.mouse << endl << endl;
+        gs = g.mouse_move(best_mouse_move(g, b));
+        if(gs){ break; }
+        cout << "cat's turn" << endl;
+        cout << "cat: " << g.state.cat << endl;
+        cout << "mouse: " << g.state.mouse << endl << endl;
+        gs = g.cat_move(best_cat_move(g, b));
+    }
+    cout << "cat: " << g.state.cat << endl;
+    cout << "mouse: " << g.state.mouse << endl;
+    cout << "game state: " << gs << endl;
+    cout << "1 = cat win, 2 = mouse win, 3 = draw" << endl;
     return 0;
 }
